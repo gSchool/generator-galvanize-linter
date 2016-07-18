5ddda3122414ded8df8ad7139f97438992ee95e6
Michael Herman <hermanmu@gmail.com>
2016-07-18 07:13:51 -0600

    init

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,29 +3,10 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
-var connect = require('gulp-connect');
 var runSequence = require('run-sequence');
 
 
 // *** tasks *** ///
-
-gulp.task('connect', function () {
-  connect.server({
-    root: './src/',
-    port: 8888,
-    livereload: true
-  });
-});
-
-gulp.task('html', function () {
-  gulp.src('./src/*.html')
-    .pipe(connect.reload());
-});
-
-gulp.task('css', function () {
-  gulp.src('./src/css/*.css')
-    .pipe(connect.reload());
-});
 
 gulp.task('javascript', function () {
   gulp.src('./src/**/*.js')
@@ -47,9 +28,11 @@ gulp.task('style', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/js/*.js', ['jshint', 'javascript', 'style']);
-  gulp.watch(['./src/*.html'], ['html']);
-  gulp.watch(['./src/css/*.css'], ['css']);
+  gulp.watch('./src/js/*.js', [
+    'jshint',
+    'javascript',
+    'style'
+  ]);
 });
 
 // *** defailt task *** //
@@ -57,7 +40,6 @@ gulp.task('default', function() {
   runSequence(
     ['jshint'],
     ['style'],
-    ['watch'],
-    ['connect']
+    ['watch']
   );
 });
